e70bb88e75f517d56d49379dd312e5621e061218
wilsonjohnson <wilson.johnson@snhu.edu>
2019-12-12 09:23:01 -0600

    Boid Object finalized

--- boids/boids.ts
+++ boids/boids.ts
@@ -4,11 +4,16 @@ import { Torus } from '../torus';
 
 export class Boid {
   public velocity: vec2;
-  public target: vec2;
-  private static IMAGE: Sprite;
+  public acceleration: vec2;
+  public turn_speed: number = 0.01;
+  public personal_bubble: number;
+  public grouping_factor: number = 100;
   private max_speed_squared: number;
+  private _max_force: number;
+  private _max_force_squared: number;
   private _view_radius: number;
   private _view_radius_squared: number;
+  private static IMAGE: Sprite;
 
   constructor(
     public position: vec2 = vec2.zero.copy(),
@@ -16,9 +21,11 @@ export class Boid {
     private max_speed: number = 2
   ) {
     this.velocity = new vec2([0,0]);
-    this.target = new vec2([0,0]);
+    this.acceleration = new vec2([0,0]);
     this.max_speed_squared = this.max_speed ** 2;
-    this.view_radius = 30;
+    this.max_force = 0.03;
+    this.view_radius = 35;
+    this.personal_bubble = 25;
     if ( ! Boid.IMAGE ) Boid.init();
   }
 
@@ -55,8 +62,26 @@ export class Boid {
     this._view_radius_squared = value;
   }
 
+  public get max_force(): number {
+    return this._max_force;
+  }
+
+  public set max_force( value: number ) {
+    this._max_force = value;
+    this._max_force_squared = value ** 2;
+  }
+
+  public get max_force_squared(): number {
+    return this._max_force_squared;
+  }
+
+  public set max_force_squared( value: number ) {
+    this._max_force_squared = value;
+    this._max_force = Math.sqrt( value );
+  }
+
   public add_velocity( value: vec2 ) {
-    this.target.add( value );
+    this.acceleration.add( value );
   }
 
   private update_grouping( others: Boid[] ) {
@@ -66,7 +91,14 @@ export class Boid {
       vector.add( Torus.offset( this.constraints, this.position, other.position ) );
     }
     vector.scale( 1 / others.length );
+    vector.subtract( this.position );
+    vector.scale( 1 / 100 );
     this.add_velocity( vector );
+  }
+
+  private limit_force( force: vec2 ): vec2 {
+    if ( force.squaredLength() < this.max_force_squared ) return force;
+
   }
 
   private update_alignment( others: Boid[] ) {
@@ -76,27 +108,35 @@ export class Boid {
       target.add( other.velocity );
     }
     target.scale( 1 / ( others.length ) );
-    target.scale(1/5);
+    target.subtract(this.velocity);
+    target.scale( 1/8 );
+    if ( target.squaredLength() > 3 ) {
+
+      target.subtract( this.velocity );
+    }
     this.add_velocity( target );
   }
 
   private update_separation( others: Boid[] ) {
     if ( others.length === 0 ) return;
-    const vector = vec2.zero.copy();
+    const target = vec2.zero.copy();
     for( let other of others ) {
       if ( other === this ) continue;
-      const between: vec2 = Torus.offset( this.constraints, other.position, this.position );
+      const between: vec2 = Torus.offset( this.constraints, this.position, other.position );
       const length = between.length();
-      if ( length < 10 )
-        vector.add( between );
+      if ( length > 0 && length < this.personal_bubble ) {
+        target.subtract( between.scale( 1 / length ) );
+      }
     }
-    this.add_velocity( vector );
+    target.scale( 1 / others.length );
+    target.subtract( this.velocity );
+    this.add_velocity( target );
   }
 
   public update_velocity() {  
-    if ( this.target.squaredLength() === 0 ) return;
-    const velocity = vec2.mix( this.velocity, this.target, 0.01 );
-    this.target.xy = this.velocity.xy;
+    if ( this.acceleration.squaredLength() === 0 ) return;
+    const velocity = vec2.mix( this.velocity, this.acceleration, this.turn_speed );
+    this.acceleration.xy = this.velocity.xy;
 
     this.velocity = velocity;
 
--- torus/torus.ts
+++ torus/torus.ts
@@ -28,6 +28,10 @@ export class Torus {
     return Torus.distance_squared( this.dimensions, from, to );
   }
 
+  public distance( from: vec2, to: vec2 ): number {
+    return Torus.distance( this.dimensions, from, to );
+  }
+
   public static distance_squared( dimensions: vec2, from: vec2, to: vec2 ): number {
     let delta = new vec2( to.copy().subtract(from).xy.map(Math.abs) as Vec2Array );
 
@@ -35,5 +39,9 @@ export class Torus {
     if ( delta.y > dimensions.y / 2 ) delta.y = dimensions.y - delta.y;
 
     return delta.squaredLength();
+  }
+
+  public static distance( dimensions: vec2, from: vec2, to: vec2 ): number {
+    return Math.sqrt(Torus.distance_squared( dimensions, from, to ));
   }
 }
